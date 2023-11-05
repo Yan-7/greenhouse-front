c7b1becdfcc0cb19b413615b806f68153843c4d1
Yan <yanivalmagor@gmail.com>
2023-11-05 20:16:48 +0200

    5.11

--- src/GreenHouseInfo.js
+++ src/GreenHouseInfo.js
@@ -1,13 +1,18 @@
+// Import necessary libraries
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+// import SockJS from 'sockjs-client';
+// import { Client } from '@stomp/stompjs';  // Updated import for STOMP client
+
 
 const GreenHouse = () => {
-    const [greenHouseData, setGreenHouseData] = useState(null);
+    const [greenHouseData, setGreenHouseData] = useState(null);  // State to hold GreenHouse data
     const [newWaterLevel, setNewWaterLevel] = useState('');  // New state variable for the new water level
     const [newFertilizeLevel, setNewFertilizeLevel] =useState('');
     const [newLightLevel, setnewLightLevel] = useState('');
 
     useEffect(() => {
+        // Existing Axios call to initially load GreenHouse data
         axios.get(`/api/green-house/1`)
             .then(response => {
                 console.log("response axios green-house: ", response.data);
@@ -16,22 +21,42 @@ const GreenHouse = () => {
             .catch(error => {
                 console.error('There was an error fetching the Green House', error);
             });
+
+        // // New WebSocket connection setup
+        // const socket = new SockJS('/ws');
+        // const stompClient = new Client({
+        //     webSocketFactory: () => socket,  // Updated STOMP client setup
+        // });
+
+        // stompClient.onConnect = (frame) => {
+        //     stompClient.subscribe('/topic/greenHouse', (update) => {
+        //         setGreenHouseData(JSON.parse(update.body));
+        //     });
+        // };
+
+        // stompClient.activate();  // Updated connection method
+
+        // // Cleanup function to disconnect the STOMP client when the component is unmounted
+        // return () => {
+        //     stompClient.deactivate();  // Updated disconnection method
+        // };
     }, []);
 
     const handleUpdateWaterLevel = () => {
-        axios.put(`/api/green-house/1/water-level`, { newLevel: newWaterLevel })  // Assuming newLevel is the parameter name expected by the backend
+        axios.put(`/api/green-house/1/water-level`, { newLevel: newWaterLevel })
             .then(response => {
                 console.log('Water level updated: ', response.data);
-                setGreenHouseData(response.data);  // Update the state with the new GreenHouse data
+                setGreenHouseData(response.data);
             })
             .catch(error => {
                 console.error('There was an error updating the water level: ', error);
             });
     };
+
     const handleUpdateFertilizeLevel = () => {
         axios.put(`/api/green-house/1/fertilize-level`, { newLevel: newFertilizeLevel })
         .then(response => {
-            setNewFertilizeLevel(response.data);
+            setGreenHouseData(response.data);
             console.log("fertilizer level updated to: " + response.data);
         })
         .catch(error => {
@@ -40,9 +65,9 @@ const GreenHouse = () => {
     }
 
     const handleUpdateLightLevel = () => {
-        axios.put(`/api/green-house/1/light-level`)
+        axios.put(`/api/green-house/1/light-level`, {newLevel: newLightLevel})
         .then(response => {
-            setnewLightLevel(response.data)
+            setGreenHouseData(response.data);
             console.log("light level updated to: " + response.data);
         })
         .catch(error => {
@@ -81,7 +106,6 @@ const GreenHouse = () => {
                         placeholder="Enter new light level"
                     />
                     <button onClick={handleUpdateLightLevel}>Update light Level</button>
-                    
                 </div>
             ) : (
                 <p>Loading GreenHouse...</p>
